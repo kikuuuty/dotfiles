c3d3f4b8b2e1444a90fb7ca086d38cda39496d02
kikuuuty <kikuuuty@gmail.com>
2015-01-19 18:46:49 +0900

    no message

--- mkshortcut.bat
+++ mkshortcut.bat
@@ -1,5 +1,6 @@
 
+mkdir %HOMEPATH%"\.vim"
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
-exit 0
 
