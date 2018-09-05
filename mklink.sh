edd39a567f0c72dd9b3748782b53faf9cb8bdcbe
kikuuuty <kikuuuty@gmail.com>
2018-09-05 11:48:25 +0900

    ftpluginのシンボリックリンクを作成しないように

--- mklink.sh
+++ mklink.sh
@@ -4,6 +4,5 @@ ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 mkdir ~/.vim
 ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/ftdetect ~/.vim/ftdetect
-ln -sf ~/dotfiles/ftplugin ~/.vim/ftplugin
 ln -sf ~/dotfiles/plugin ~/.vim/plugin
 ln -sf ~/dotfiles/syntax ~/.vim/syntax
