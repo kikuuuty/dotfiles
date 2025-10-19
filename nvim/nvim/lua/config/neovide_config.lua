fea70238015737450d68ef09a72fb3ed8eb62cda
kikuuuty <kikuuuty@gmail.com>
2025-10-19 16:05:51 +0900

    neovideの影を削除

--- nvim/nvim/lua/config/neovide_config.lua
+++ nvim/nvim/lua/config/neovide_config.lua
@@ -45,11 +45,11 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 
 
 -- Floating Blur Amount
---vim.g.neovide_floating_blur_amount_x = 2.0
---vim.g.neovide_floating_blur_amount_y = 2.0
+vim.g.neovide_floating_blur_amount_x = 0.0--2.0
+vim.g.neovide_floating_blur_amount_y = 0.0--2.0
 
 -- Floating Shadow
---vim.g.neovide_floating_shadow = true
+vim.g.neovide_floating_shadow = false--true
 --vim.g.neovide_floating_z_height = 10
 --vim.g.neovide_light_angle_degrees = 45
 --vim.g.neovide_light_radius = 5
