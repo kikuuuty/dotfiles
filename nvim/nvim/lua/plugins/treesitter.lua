56c6fd441eedf7347de4539f82bad9a9fb582aa5
kikuuuty <kikuuuty@gmail.com>
2025-10-28 00:42:46 +0900

    treesitterで幾つかの言語をインストールするように

--- nvim/nvim/lua/plugins/treesitter.lua
+++ nvim/nvim/lua/plugins/treesitter.lua
@@ -9,6 +9,8 @@
                 additional_vim_regex_highlighting = false,
             },
             indent = { enable = true },
+            auto_install = false,
+            ensure_installed = { "c", "cpp", "lua", "python", "json" },
         })
     end,
 }
