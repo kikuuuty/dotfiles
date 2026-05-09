dc7c502aa65b06dd38c82ca55cb5bf4ef22d0282
kikuuuty <kikuuuty@gmail.com>
2026-05-09 16:52:51 +0900

    nvimのC++コンストラクタイニシャライザのインデントを修正
    コンストラクタのイニシャライザリストと本体を、クラス内外どちらの
    定義でも宣言行基準で揃えるようにする。

--- nvim/nvim/after/ftplugin/cpp.lua
+++ nvim/nvim/after/ftplugin/cpp.lua
@@ -30,6 +30,86 @@ local function matching_initializer_indent(lnum)
   return nil
 end
 
+local function is_constructor_declaration(line)
+  return line:match('%)%s*[%w_:&*<>%s]*$') ~= nil
+end
+
+local function constructor_initializer_indent(lnum)
+  local line = vim.fn.getline(lnum)
+  local first_initializer = lnum
+
+  if line:match('^%s*$') then
+    local prev = vim.fn.prevnonblank(lnum - 1)
+    if prev == 0 or not vim.fn.getline(prev):match('^%s*[:,]') then
+      return nil
+    end
+    first_initializer = prev
+  elseif not line:match('^%s*[:,]') then
+    return nil
+  end
+
+  while first_initializer > 1 do
+    local prev = vim.fn.prevnonblank(first_initializer - 1)
+    if prev == 0 or not vim.fn.getline(prev):match('^%s*[:,]') then
+      break
+    end
+    first_initializer = prev
+  end
+
+  local declaration = vim.fn.prevnonblank(first_initializer - 1)
+  if declaration == 0 or not is_constructor_declaration(vim.fn.getline(declaration)) then
+    return nil
+  end
+
+  return vim.fn.indent(declaration) + vim.fn.shiftwidth()
+end
+
+local function constructor_body_indent(lnum)
+  local stack = {}
+
+  for i = 1, lnum - 1 do
+    local line = vim.fn.getline(i)
+    for col = 1, #line do
+      local char = line:sub(col, col)
+      if char == '{' then
+        table.insert(stack, { line = i })
+      elseif char == '}' then
+        table.remove(stack)
+      end
+    end
+  end
+
+  if #stack == 0 then
+    return nil
+  end
+
+  local open_line_number = stack[#stack].line
+  local open_line = vim.fn.getline(open_line_number)
+  if not open_line:match('^%s*[:,]') or not open_line:match('{%s*$') then
+    return nil
+  end
+
+  local first_initializer = open_line_number
+  while first_initializer > 1 do
+    local prev = vim.fn.prevnonblank(first_initializer - 1)
+    if prev == 0 or not vim.fn.getline(prev):match('^%s*[:,]') then
+      break
+    end
+    first_initializer = prev
+  end
+
+  local declaration = vim.fn.prevnonblank(first_initializer - 1)
+  if declaration == 0 or not is_constructor_declaration(vim.fn.getline(declaration)) then
+    return nil
+  end
+
+  if vim.fn.getline(lnum):match('^%s*}') then
+    return vim.fn.indent(declaration)
+  end
+
+  return vim.fn.indent(declaration) + vim.fn.shiftwidth()
+end
+
 local function is_lambda_opening(line, col)
   local before = line:sub(1, col - 1)
   return before:match('%b[]%s*$') ~= nil
@@ -100,7 +180,7 @@ local function cpp_indent()
   local lnum = vim.v.lnum
   local indent = vim.fn.cindent(lnum)
 
-  indent = matching_initializer_indent(lnum) or lambda_body_indent(lnum) or indent
+  indent = matching_initializer_indent(lnum) or constructor_initializer_indent(lnum) or constructor_body_indent(lnum) or lambda_body_indent(lnum) or indent
 
   if lnum > 1 then
     local prev = vim.fn.prevnonblank(lnum - 1)
